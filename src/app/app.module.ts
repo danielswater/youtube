d2809a0f6e273a7066bc40651790583d731f5284
Daniel Swater <danielswater@gmail.com>
2017-07-29 10:43:54 -0300

    acesso ao codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
+import { YoutubeService } from './../pages/home/youtube';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SafePipe } from '../pages/home/safe.pipe';
+//import { AutoCompleteModule } from 'ionic2-auto-complete';
+import { AutoCompleteModule } from '../pages/home/component/index';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SafePipe
   ],
   imports: [
     BrowserModule,
+    AutoCompleteModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -23,6 +32,7 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    YoutubeService,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
+import { Observable } from 'rxjs/Observable';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { YoutubeService } from './youtube';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	search = new FormControl();
+	results: Observable<any>;
 
-  }
+	constructor(public navCtrl: NavController, public youtube: YoutubeService) {
+		/* this.search.valueChanges
+			.debounceTime(200)
+			.switchMap(query => youtube.search(query))
+			.subscribe(result => {
+				this.results = result;
+				console.log(result);
+			}) */
+	}
+	
 
 }
